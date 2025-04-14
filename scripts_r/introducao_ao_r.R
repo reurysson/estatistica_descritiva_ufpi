9ae885a87af4d39d4a8fa5a929b2c7d03a11ccff
Reurysson Morais <68240793+reurysson@users.noreply.github.com>
2025-04-14 13:08:52 -0300

    Update

---
+++ scripts_r/introducao_ao_r.R
@@ -0,0 +1,17 @@
+# Comandos básicos no R
+
+getwd() # Get Working Directory - Exibe o diretório de trabalho atual
+
+setwd() # Set (settings) Working Directory - Permite alterar o diretório de trabalho. # O caminho no novo diretório é passado como argumento. 
+# Atenção  para o uso das "\\" ou "/". No R "\" é um caractere de scape.
+
+
+setwd("C:\\Users\\reury\\OneDrive\\Estatistica_UFPI\\estatistica_descritiva\\pratica_no_r") 
+
+getwd()
+
+
+
+x <- c(3,4,5)
+x
+ls()
